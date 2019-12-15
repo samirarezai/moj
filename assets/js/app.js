60c952e7250c3ce20175f43b9d1c812ba0a27672
samira <samirarezai1996@gmail.com>
2019-12-15 17:57:08 +0330

    add team

--- assets/js/app.js
+++ assets/js/app.js
@@ -131,3 +131,123 @@ particlesJS('particles-js',
     }
 
 );
+particlesJS('particles_js',
+
+    {
+      "particles": {
+        "number": {
+          "value": 30,
+          "density": {
+            "enable": true,
+            "value_area": 800
+          }
+        },
+        "color": {
+          "value": "#FFEE00"
+        },
+        "shape": {
+          "type": "circle",
+          "stroke": {
+            "width": 0,
+            "color": "rgb(172,177,65)"
+          },
+          "polygon": {
+            "nb_sides":3
+          },
+          "image": {
+            "src": "img/github.svg",
+            "width": 100,
+            "height": 100
+          }
+        },
+        "opacity": {
+          "value": 0.35,
+          "random": false,
+          "anim": {
+            "enable": false,
+            "speed": 10,
+            "opacity_min": 0.001,
+            "sync": false
+          }
+        },
+        "size": {
+          "value": 5,
+          "random": true,
+          "anim": {
+            "enable": true,
+            "speed": 3,
+            "size_min": 0.001,
+            "sync": false
+          }
+        },
+        "line_linked": {
+          "enable": false,
+          "distance": 0,
+          "color": "#ffffff",
+          "opacity": 0.4,
+          "width": 1
+        },
+        "move": {
+          "enable": true,
+          "speed": 2,
+          "direction": "none",
+          "random": true,
+          "straight": false,
+          "out_mode": "out",
+          "attract": {
+            "enable": false,
+            "rotateX": 600,
+            "rotateY": 1200
+          }
+        }
+      },
+      "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+          "onhover": {
+            "enable": true,
+            "mode": "repulse"
+          },
+          "onclick": {
+            "enable": true,
+            "mode": "push"
+          },
+          "resize": true
+        },
+        "modes": {
+          "grab": {
+            "distance": 400,
+            "line_linked": {
+              "opacity": 1
+            }
+          },
+          "bubble": {
+            "distance": 400,
+            "size": 40,
+            "duration": 2,
+            "opacity": 8,
+            "speed": 3
+          },
+          "repulse": {
+            "distance": 200
+          },
+          "push": {
+            "particles_nb": 4
+          },
+          "remove": {
+            "particles_nb": 2
+          }
+        }
+      },
+      "retina_detect": false,
+      "config_demo": {
+        "hide_card": true,
+        "background_color": "#000",
+        "background_image": "",
+        "background_position": "50% 50%",
+        "background_repeat": "no-repeat",
+        "background_size": "cover"
+      }
+    }
+
+);
--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -4,26 +4,60 @@ var isFireFox = navigator.userAgent.indexOf("Firefox");
 $header_top.find('a').on('click', function () {
     $(this).parent().toggleClass('open-menu');
 });
+if ($(window).width() < 768) {
+    var height = window.innerHeight ? window.innerHeight : $(window).height();
+    $('.head-section').css('min-height', height + 'px');
+    $('.vertical-scrolling').css('min-height', height + 'px');
+    $('.vertical-scrolling').css('height', 'auto');
+    /*control change height of activities*/
+    $('.activities-app .carousel-inner').each(function () {
+        var highestBox = 0;
+        $('.carousel-item', this).each(function () {
+            if ($(this).height() > highestBox) {
+                highestBox = $(this).height();
+            }
 
+        });
+
+        if (isFireFox > 0) {
+            $('.carousel-item', this).height('auto');
+            $('.vertical-scrolling.changeVariable').css('min-height', highestBox + 'px');
+            $('.box-activities').css('min-height', highestBox + 'px');
+            $('.box-activities .carousel-inner').css('min-height', highestBox + 'px');
+            $('.box-systems .carousel-inner').css('min-height', highestBox + 70);
+            $('.carousel-item', this).css('min-height', highestBox);
+            $('.activities-property').height(highestBox);
+            $('.activities-gallery').height(highestBox);
+        } else {
+            $('.carousel-item', this).height(highestBox);
+            $('.activities-property').height(highestBox);
+            $('.activities-gallery').height(highestBox);
+        }
+
+    });
+    /*control change height of text box system*/
+    var firstHeight = $('.system-first-text').height();
+    $('.system-box-text').height(firstHeight + 20);
+}
 $('#fullpage').fullpage({
-    sectionsColor: ['#fff', '#fff', '#fff', '#fff', '#fff'],
+    sectionsColor: ['#fff', '#fff', '#fff', '#fff', '#fff', '#fff'],
     sectionSelector: '.vertical-scrolling',
     navigation: true,
     slidesNavigation: true,
     controlArrows: false,
-    anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection'],
+    anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection', 'sixthSection'],
     menu: '#menu',
     responsiveWidth: 768,
     afterLoad: function (anchorLink, index) {
         $header_top.css('background', 'rgba(0, 47, 77, .3)');
         $nav.css('background', 'rgba(0, 47, 77, .25)');
-        if (index == 5) {
+        if (index == 6) {
             $('#fp-nav').hide();
         }
     },
 
     onLeave: function (index, nextIndex, direction) {
-        if (index == 5) {
+        if (index == 6) {
             $('#fp-nav').hide();
         }
     },
@@ -79,42 +113,7 @@ $('#fullpage').fullpage({
     }
 
 });
-if ($(window).width() < 768) {
-    var height = window.innerHeight ? window.innerHeight : $(window).height();
-    $('.head-section').css('min-height', height + 'px');
-    $('.vertical-scrolling').css('min-height', height + 'px');
-    $('.vertical-scrolling').css('height', 'auto');
-    /*control change height of activities*/
-    $('.activities-app .carousel-inner').each(function () {
-        var highestBox = 0;
-        $('.carousel-item', this).each(function () {
-            console.log($(this).height())
-            if ($(this).height() > highestBox) {
-                highestBox = $(this).height();
-            }
 
-        });
-
-        if (isFireFox > 0) {
-            $('.carousel-item', this).height('auto');
-            $('.vertical-scrolling.changeVariable').css('min-height', highestBox + 'px');
-            $('.box-activities').css('min-height', highestBox + 'px');
-            $('.box-activities .carousel-inner').css('min-height', highestBox + 'px');
-            $('.box-systems .carousel-inner').css('min-height', highestBox + 70);
-            $('.carousel-item', this).css('min-height', highestBox);
-            $('.activities-property').height(highestBox);
-            $('.activities-gallery').height(highestBox);
-        } else {
-            $('.carousel-item', this).height(highestBox);
-            $('.activities-property').height(highestBox);
-            $('.activities-gallery').height(highestBox);
-        }
-
-    });
-    /*control change height of text box system*/
-    var firstHeight = $('.system-first-text').height();
-    $('.system-box-text').height(firstHeight + 20);
-}
 $('.bars').click(function () {
     if ($(this).attr('data-open') == 'open') {
         $(this).find('.hamburger').addClass('opened');
@@ -127,7 +126,7 @@ $('.bars').click(function () {
     }
 });
 /*==================owlCarousel====================*/
-var owl = $('.owl-carousel');
+var owl = $('.gallary-owl-carousel');
 owl.owlCarousel({
     items: 3,
     loop: true,
@@ -154,8 +153,34 @@ owl.owlCarousel({
 });
 $(".owl-prev").html('<img src="assets/img/next.svg" alt="">');
 $(".owl-next").html('<img src="assets/img/next.svg" alt="">');
-
-
+/*==================teamOwlCarousel====================*/
+var team_owl = $('.team-owl-carousel');
+team_owl.owlCarousel({
+    items: 5,
+    loop: true,
+    margin: 0,
+    nav: true,
+    rtl: true,
+    center: true,
+    autoplay: 3000,
+    slideSpeed: 200,
+    autoplayHoverPause: true,
+    scrollPerPage: true,
+    smartSpeed: 1000,
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items:3,
+        },
+        1200: {
+            items: 5
+        }
+    }
+});
+$(".team-owl-carousel .owl-prev").html('<img src="assets/img/arrow-pointing-to-right.svg" alt="">');
+$(".team-owl-carousel .owl-next").html('<img src="assets/img/arrow-pointing-to-right.svg" alt="">');
 /*==================magnific-popup====================*/
 $('.owl-image').magnificPopup({
     type: 'image',
